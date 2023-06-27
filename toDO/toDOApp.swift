2b18a0302244bfff234a3172cf5c83321ba7636f
clover <atkinsonclover4@gmail.com>
2023-06-27 10:45:53 -0600

    Initial Commit

---
+++ toDO/toDOApp.swift
@@ -0,0 +1,17 @@
+//
+//  toDOApp.swift
+//  toDO
+//
+//  Created by Scholar on 6/27/23.
+//
+
+import SwiftUI
+
+@main
+struct toDOApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
